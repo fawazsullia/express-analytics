f40f1f38b3388c4f50887942885d849585cdc987
Fawaz Sullia <fawazsullia@gmail.com>
2022-01-14 18:25:55 +0400

    First: Initialize npm package

---
+++ index.js
@@ -0,0 +1,16 @@
+
+function expressAnalytics(options = {name : "express-analytics"}){
+
+    return function(req, res, next){
+    
+    
+
+
+    next()
+    }
+    
+    
+    }
+    
+    module.exports = expressAnalytics;
+    
