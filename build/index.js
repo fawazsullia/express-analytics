cde5dff000e872fc9d97b7ec520956b6d72841db
Fawaz Sullia <fawazsullia@gmail.com>
2022-01-15 22:30:50 +0400

    Add typescript support

---
+++ build/index.js
@@ -0,0 +1,14 @@
+"use strict";
+function expressAnalytics(options = { cb: (a) => { console.log("Hello"); } }) {
+    return function (req, res, next) {
+        //track non unique hits
+        let nonUniqueHits = 1;
+        console.log("Does this work man");
+        //callback is called here. The function definition for the callback is made in the server. The parameters can be 
+        //stored in a databased
+        options.cb(nonUniqueHits);
+        //proceed to the next middleware
+        next();
+    };
+}
+module.exports = expressAnalytics;
