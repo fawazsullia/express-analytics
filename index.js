9a18c1ef8b5a28692e84adbc6628bbe678f7c377
Fawaz Sullia <fawazsullia@gmail.com>
2022-01-15 21:57:35 +0400

    feature: track nonunique hits. Execute a callback on each server hit

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-
-function expressAnalytics(options = {name : "express-analytics"}){
-
-    return function(req, res, next){
-    
-    
+function expressAnalytics(options = { cb }) {
+  return function (req, res, next) {
+    //track non unique hits
+    let nonUniqueHits = 1;
 
 
-    next()
-    }
-    
-    
-    }
-    
-    module.exports = expressAnalytics;
-    
+
+
+    //callback is called here. The function definition for the callback is made in the server. The parameters can be 
+    //stored in a databased
+    options.cb(nonUniqueHits);
+
+    //proceed to the next middleware
+    next();
+  };
+}
+
+module.exports = expressAnalytics;
